a294437324cd7c31347fcc5a030e79ce6d8ab486
ompy <ompysaini1999@gmail.com>
2024-09-10 23:41:12 +0530

    new build

--- HospitalManagement/hospitalBackend/connection.js
+++ HospitalManagement/hospitalBackend/connection.js
@@ -1,9 +1,21 @@
 
+// const mongoose = require('mongoose');
+// try {
+//     mongoose.connect("mongodb://127.0.0.1:27017/hospital").then(()=> {
+//         console.log("connection created with db");
+//     })
+// } catch(err) {
+//     console.log("connection failed------", err);
+// }
+
 const mongoose = require('mongoose');
-try {
-    mongoose.connect("mongodb://127.0.0.1:27017/hospital").then(()=> {
-        console.log("connection created with db");
+
+const dbUri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/hospital";
+
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connection created with DB");
     })
-} catch(err) {
-    console.log("connection failed------", err);
-}
+    .catch(err => {
+        console.log("Connection failed ------", err);
+    });
--- HospitalManagement/hospitalBackend/index.js
+++ HospitalManagement/hospitalBackend/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 require('./connection');
-const serverless = require('serverless-http');
 
 const patientRouter = require('./routes/patientRoute');
 const dARRouter = require('./routes/docAdmRecRoute');
@@ -16,9 +15,7 @@ app.use(express.static(path.join(__dirname,'dist/hospital-frontend')));
 app.use(patientRouter);
 app.use(dARRouter);
 
-app.use('/.netlify/functions/api', app);
 app.listen(port, ()=> {
     console.log("server running on port no. ",port);
 });
-module.exports.handler = serverless(app);
 
