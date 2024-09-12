1aa289f32e869fc5c002192d47269ccc4a05adfb
ompy <ompysaini1999@gmail.com>
2024-09-12 19:21:17 +0530

    new build

--- HospitalManagement/hospitalBackend/connection.js
+++ HospitalManagement/hospitalBackend/connection.js
@@ -10,7 +10,8 @@
 
 const mongoose = require('mongoose');
 
-const dbUri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/hospital";
+// const dbUri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/hospital";
+const dbUri = 'mongodb+srv://ompysaini1999:ompy%401999@cluster0.qokls.mongodb.net/hospital?retryWrites=true&w=majority';
 
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
