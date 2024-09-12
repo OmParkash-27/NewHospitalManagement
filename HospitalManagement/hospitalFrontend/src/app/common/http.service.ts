bb0de2454e2eb1cda1dbf4b0ae4eec23540b1a7b
ompy <ompysaini1999@gmail.com>
2024-09-12 14:45:59 +0530

    new build

--- HospitalManagement/hospitalFrontend/src/app/common/http.service.ts
+++ HospitalManagement/hospitalFrontend/src/app/common/http.service.ts
@@ -1,12 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  serverUrl = "http://127.0.0.1:8000/";
+  // serverUrl = "http://127.0.0.1:8000/";
+  private serverUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
