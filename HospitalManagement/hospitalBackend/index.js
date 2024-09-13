05f3d256c98f6e4deedd8608db4128f4391002e2
ompy <ompysaini1999@gmail.com>
2024-09-13 23:03:36 +0530

    new build

--- HospitalManagement/hospitalBackend/index.js
+++ HospitalManagement/hospitalBackend/index.js
@@ -12,9 +12,9 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname,'dist/hospital-frontend')));
 // Catch-all route to send index.html for unknown paths
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/hospital-frontend/index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'dist/hospital-frontend/index.html'));
+//   });
 app.use(patientRouter);
 app.use(dARRouter);
 
