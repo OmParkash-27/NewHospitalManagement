8791c04f38d1105395a9d043534666b552411b3b
ompy <ompysaini1999@gmail.com>
2024-09-13 22:12:18 +0530

    new build

--- HospitalManagement/hospitalBackend/connection.js
+++ HospitalManagement/hospitalBackend/connection.js
@@ -13,9 +13,7 @@ const mongoose = require('mongoose');
 const dbUri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/hospital";
 // const dbUri = 'mongodb+srv://ompysaini1999:ompy%401999@cluster0.qokls.mongodb.net/hospital?retryWrites=true&w=majority';
 
-console.log("MONGODB_URI: ", process.env.MONGODB_URI);
-
-mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbUri)
     .then(() => {
         console.log("Connection created with DB");
     })
